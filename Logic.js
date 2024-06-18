c3b1e962e18c01047aaa6499b1f3f5841f42f8a4
yashchokhat <155607086+yashchokhat@users.noreply.github.com>
2024-06-18 17:04:47 +0530

    Add files via upload

---
+++ Logic.js
@@ -0,0 +1,9 @@
+let btn = document.querySelector("#Project");
+let btn1 = document.querySelector("#btn01")
+btn.onmouseover = () => {
+    alert('Project Section is Under Development Phase');
+};
+
+btn1.onclick = () => {
+    alert('Re-Directing to Youtube Chaneel');
+};
